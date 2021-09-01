6e308e3ac5cd295ff1118c7d06fe9154b9407d7c
BAM-BCEM-KAPYT <43520508+After2Dark@users.noreply.github.com>
2021-09-01 20:39:59 +0300

    Init rep

---
+++ Blue Lamp/Debug/Src/main.d
@@ -0,0 +1,24 @@
+Src/main.o: ../Src/main.c ../Inc/stm32f746xx.h ../Inc/core_cm7.h \
+ ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h ../Inc/cmsis_gcc.h \
+ ../Inc/mpu_armv7.h ../Inc/system_stm32f7xx.h ../Src/one_picture.h \
+ ../Src/init_picture.h ../Src/main_picture.h
+
+../Inc/stm32f746xx.h:
+
+../Inc/core_cm7.h:
+
+../Inc/cmsis_version.h:
+
+../Inc/cmsis_compiler.h:
+
+../Inc/cmsis_gcc.h:
+
+../Inc/mpu_armv7.h:
+
+../Inc/system_stm32f7xx.h:
+
+../Src/one_picture.h:
+
+../Src/init_picture.h:
+
+../Src/main_picture.h:
